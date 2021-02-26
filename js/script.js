1c74b5c0297f57e1e200c1d5d07d1cbffc397594
Andrea <andrea3aprile99@gmail.com>
2021-02-26 20:20:08 +0100

    f

--- js/script.js
+++ js/script.js
@@ -2,9 +2,8 @@
 
 var parola1=prompt("inserisci quanti km devi percorrere");
 var parola2=prompt("inserisci la tua eta");
-var prezzo3 = "prezzo totale : " + prezzo2 ;
 var prezzo2 =  0.21 * parola1 ;
-var prezzo= alert( prezzo2 + "$" ) ;
+var prezzo= alert( "prezzo totale : " + prezzo2 + "$" ) ;
 
 if ( isNaN(parola1) ) {
   parola1 = parseInt(prompt("inserisci km in numero "));
@@ -17,8 +16,8 @@ if ( isNaN(parola2) ) {
 
 
  if ( parola2 < 18 ){
-  alert (prezzo2 * 0.8);
+  alert( "prezzo scontato : " + (prezzo2 * 0.8) + "$" );
  }
  else if ( parola2 > 65 ){
-  alert(prezzo2 * 0.6);
+  alert( "prezzo scontato : " + (prezzo2 * 0.6) + "$" );
  }
